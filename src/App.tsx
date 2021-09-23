856ed966a65de50d9601b005f172f7b3cba5d40c
gabrikf <gabrielkochf@gmail.com>
2021-09-22 21:51:47 -0300

    chages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import Button from '@material-ui/core/Button'
+
 import Routes from './routes'
 
 
 function App() {
   return (
  <Routes />
-   
   );
 }
 
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,26 @@
+import { Button } from "@mui/material";
+import TextField from "@mui/material/TextField";
+import { Box } from "@mui/system";
 
 const Home = () => {
-    return (
-       <h1>opa</h1>
-    )
-}
-export default Home
+  return (
+    <>
+    <Box >
+       <Box
+      component="form"
+      sx={{ display: 'flex' }}
+      noValidate
+      alignItems="center"
+      justifyContent="center"
+      autoComplete="off"
+    >
+        <TextField id="outlined-basic" label="Número de perguntas" variant="outlined" />
+        <Button variant="contained" color="primary">
+          Buscar
+        </Button>
+      </Box>
+      </Box>
+    </>
+  );
+};
+export default Home;
