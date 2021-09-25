fe8120ac3c2f834ab9a5d6c84748b19f22b2cd88
gabrikf <gabrielkochf@gmail.com>
2021-09-24 21:42:05 -0300

    new checkpoint

--- src/pages/Questions/index.tsx
+++ src/pages/Questions/index.tsx
@@ -5,6 +5,8 @@ import {
   Radio,
   RadioGroup,
   Button,
+  Box,
+  Grid,
 } from "@material-ui/core";
 import api from "../../services/api";
 import { useState } from "react";
@@ -17,20 +19,31 @@ type QuestionsType = {
   correct_answer: string;
   difficulty: string;
   incorrect_answers: string[];
+  answers: string[];
   question: string;
   type: string;
 };
 
 const Questions = () => {
   const [buttons, setButtons] = useState(true);
-  const [quanstions, setQuestions] = useState<QuestionsType[]>([]);
+  const [questions, setQuestions] = useState<QuestionsType[]>([]);
   const history = useHistory();
   const { quantity } = useQuestions();
   const handleStartQuestions = async () => {
     setButtons(false);
-    await api.get(`api.php?amount=${quantity}. `).then((response) => {
-      setQuestions(response.data);
-      console.log(response.data.results);
+    await api.get(`api.php?amount=${quantity}`).then((response) => {
+      let newQuestions = response.data.results.map((value: any) => {
+        let result = value.incorrect_answers;
+
+        result.push(value.correct_answer);
+
+        return {
+          ...value,
+          answers: result.sort((v1: any, v2: any) => v1.localeCompare(v2)),
+        };
+      });
+
+      setQuestions(newQuestions);
     });
   };
 
@@ -62,31 +75,30 @@ const Questions = () => {
           </div>
         </div>
       ) : (
-        <>
-          {quanstions.map((item : QuestionsType) => (
-            <div className="flex-item">
-              <FormControl component="fieldset">
-                <FormLabel component="legend">Gender</FormLabel>
-                <RadioGroup
-                  aria-label="gender"
-                  defaultValue="female"
-                  name="radio-buttons-group"
-                >
-                  <FormControlLabel
-                    value="female"
-                    control={<Radio />}
-                    label="Female"
-                  />
-                  <FormControlLabel
-                    value="male"
-                    control={<Radio />}
-                    label="Male"
-                  />
-                </RadioGroup>
-              </FormControl>
-            </div>
-          ))}
-        </>
+        <Box sx={{ flexGrow: 1 }}>
+          <Grid container spacing={2}>
+            {questions.map((item: QuestionsType, index: Number) => (
+              <Grid item xs={6} sm={6} md={6}>
+                <FormControl component="fieldset">
+                  <FormLabel component="legend">Gender</FormLabel>
+                  <RadioGroup
+                    aria-label="gender"
+                    defaultValue="female"
+                    name="radio-buttons-group"
+                  >
+                    {item.answers.map((value: any, index: Number) => (
+                      <FormControlLabel
+                        value={index.toString()}
+                        control={<Radio />}
+                        label={value}
+                      />
+                    ))}
+                  </RadioGroup>
+                </FormControl>
+              </Grid>
+            ))}
+          </Grid>
+        </Box>
       )}
     </>
   );
